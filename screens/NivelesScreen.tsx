6d81cb30cacdede243d3cb6b59a6ee2b1baee152
JohanAlzaGitHub <johanalza.github@gmail.com>
2024-01-10 19:28:29 -0500

    Screen Niveles

--- screens/NivelesScreen.tsx
+++ screens/NivelesScreen.tsx
@@ -1,10 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function NivelesScreen() {
+const NivelesScreen = ({ navigation }: any) => {
+
+  const startGame = (difficulty: string) => {
+    navigation.navigate('Juegos', { difficulty });
+  };
+
   return (
-    <View>
-      <Text>NivelesScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Seleccione la dificultad:</Text>
+      <Button title="Fácil" onPress={() => startGame('easy')} style={styles.button} />
+      <Button title="Moderado" onPress={() => startGame('medium')} style={styles.button} />
+      <Button title="Difícil" onPress={() => startGame('hard')} style={styles.button} />
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f0f0f0', // Cambia el color de fondo según tus preferencias
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginVertical: 10,
+  },
+});
+
+export default NivelesScreen;
